7ab1dc0bbf96011dafc153718f68776fd812b180
Murvinator <jonathanbelloni@gmail.com>
2026-02-27 14:14:49 +0100

    Settings header fix (tog oanvändbara tillbaka-knappen)

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -2,11 +2,11 @@
   <div class="settings-view-root">
     <div class="settings-header">
       <div class="settings-header-left">
-        <button class="back-btn" @click="goBack()" title="Tillbaka">
+        <!-- <button class="back-btn" @click="goBack()" title="Tillbaka">
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5">
             <polyline points="15 18 9 12 15 6"></polyline>
           </svg>
-        </button>
+        </button> -->
         <h2>Inställningar</h2>
       </div>
     </div>
